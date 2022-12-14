87b8d7baa265af1d02c91d9c7d76e2f41cdbedcb
WiyangLautner <wiyanglautner11@gmail.com>
2022-12-14 16:23:59 +0700

    Deploy gh-pages

--- ui/out/_next/static/chunks/webpack-2acc03f1d4c94b97.js
+++ ui/out/_next/static/chunks/webpack-2acc03f1d4c94b97.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "57981f755a53415e" + ".css";
+/******/ 			return "static/css/" + "d7839e6da5291faf" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
